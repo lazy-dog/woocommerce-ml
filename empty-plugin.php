c8182a7ec924ab413a7629837490fc90a1767b7f
Karl <karlmaw@hotmail.com>
2019-10-15 18:12:59 +0100

    Initial

---
+++ empty-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Empty plugin
+ * Plugin URI: 
+ * Text Domain: empty-plugin
+ * Description: A fresh start
+ * Version: 1.0
+ * Author: You
+ * Author URI: 
+ * License: GPL2
+ *
+ */
+
+namespace EmptyPlugin;
+
+if (!defined('WPINC')) {
+    die();
+}
